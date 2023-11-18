961d4b455f94d3be55aec545800a200e92d2991b
Egor Matuk <gregory.matuk2004@gmail.com>
2023-11-19 01:02:19 +0200

    💡 Added docstring to generate_qr_code function

--- src/services/qr_generation/generate_qr_code.py
+++ src/services/qr_generation/generate_qr_code.py
@@ -1,6 +1,13 @@
 from qrcode import make
 
-def generate_qr_code(data: str, filename: str):
+def generate_qr_code(data: str, destination_path: str):
+    """
+    Create qr code and save it
+
+    Args:
+        data (str): string to encode into a qr coee
+        destination_path (str): path where we're saving the qr code
+    """
     qr = make(data)
-    qr.save(filename)
+    qr.save(destination_path)
     
