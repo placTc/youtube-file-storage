d2e6251557c97d5f7b6657415c448deeb7e16905
Egor Matuk <gregory.matuk2004@gmail.com>
2023-11-19 02:06:58 +0200

    🚑 oopsie daisies

--- src/services/conversion/__init__.py
+++ src/services/conversion/__init__.py
@@ -1,4 +1,4 @@
-from services.encoding.base64_converter import base64_converter_factory, base64_converter
-from services.encoding.converter import Converter
+from services.conversion.base64_converter import base64_converter_factory, base64_converter
+from services.conversion.interface import Converter
 
 __all__ = ["Converter", "base64_converter_factory", "base64_converter"]
